a63cedb64b9140f4a7b2ec6d5890b39764466e3e
sim591701407 <591701407@qq.com>
2018-08-28 10:43:43 +0800

    好了

--- spot.js
+++ spot.js
@@ -6,11 +6,13 @@
     var engine = new Shape.Engine(canvas);
     var promise = new Promise((resolve) => { resolve(); });
     
-    document.getElementById('go');
+    document.getElementById('go').addEventListener('click',start);
     
     function start() {
         document.getElementById('go').removeEventListener('click', start);
-        promise.then(() => engine.toText('L'))
+        promise.then(() => engine.toText('I'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('L'))
         .then(() => engine.shake())
         .then(() => engine.toText('O'))
         .then(() => engine.shake())
@@ -18,7 +20,22 @@
         .then(() => engine.shake())
         .then(() => engine.toText('E'))
         .then(() => engine.shake())
+		.then(() => engine.toText('Y'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('O'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('U'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('两年前写的'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('然而现在'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('还是没有女朋友'))
+        .then(() => engine.shake())
+		.then(() => engine.toText('T-T！'))
+        .then(() => engine.shake())
         .then(() => engine.clear())
         .then(() => document.getElementById('go').addEventListener('click', start));
     }
+	.start()
 })();
